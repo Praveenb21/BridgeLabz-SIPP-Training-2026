8a43701f9d10188c002a916c86e064d490961dbc
Praveen Bhagour <168171538+Praveenb21@users.noreply.github.com>
2026-06-30 09:11:11 +0530

    Delete core-java-practice directory

--- core-java-practice/gcr-codebase/programming-elements/DistanceToMiles.java
+++
@@ -1,8 +0,0 @@
-class DistanceToMiles {
-    public static void main(String[] args) {
-        double kiloDistance = 10.8;
-
-        double milesDistance = kiloDistance * 1.6;
-        System.out.println("The distance " + kiloDistance + " km in miles is " + milesDistance);
-    }
-}
